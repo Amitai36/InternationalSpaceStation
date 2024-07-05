b3f17b43b8660f29e3ecf9b09ec4e134c8e616f1
amitai36 <Amitiy36@gmail.com>
2024-07-05 17:36:17 +0300

    get Country above ISS

--- src/controllers/countries.ts
+++ src/controllers/countries.ts
@@ -16,3 +16,27 @@ export const getCountries = async (_req: Request, res: Response) => {
     res.status(500).send({ error }).end();
   }
 };
+
+export const getCountryISS = async (_req: Request, res: Response) => {
+  try {
+    const featureCollection = turf.featureCollection(geoJson.features);
+    const response = await axios.get<ISSMessage>(
+      "http://api.open-notify.org/iss-now.json"
+    );
+    const y = response.data.iss_position.latitude;
+    const x = response.data.iss_position.longitude;
+    const point = turf.point([+x, +y]);
+    console.log(point);
+    let country = "Ocean";
+    for (const feature of featureCollection.features) {
+      if (turf.booleanPointInPolygon(point, feature)) {
+        console.log("name");
+        country = feature.properties.name;
+        break;
+      }
+    }
+    res.status(200).send(country).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+  }
+};
--- src/routes/countries.ts
+++ src/routes/countries.ts
@@ -1,11 +1,8 @@
-import {
-  getCountries,
-  getCountryISS,
-  getISSLocation,
-} from "../controllers/countries";
+import { getCountries, getCountryISS } from "../controllers/countries";
 import { Router } from "express";
 
 const router = Router();
 router.get("/getCountries", getCountries);
+router.get("/getCountryISS", getCountryISS);
 
 export default router;
